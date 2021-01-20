1e9a60e642aba3224caa05ebdfdec81fa4aa2da0
m.rusakova <m.rusakova@itransition.com>
2021-01-20 23:34:43 +0300

    add modal and fix its behaviour

--- js/script.js
+++ js/script.js
@@ -59,3 +59,58 @@ function setClock(selector, endtime) {
 }
 
 setClock('.timer', deadline);
+
+const contactBtn = document.querySelectorAll('[data-modal'),
+    closeBtn = document.querySelector('[data-close'),
+    modal = document.querySelector('.modal');
+
+function showModal() {
+    modal.classList.add('active');
+     document.body.style.overflow = 'hidden';
+    //  clearInterval(modalPopTime);
+    // clearInterval(openModalTimer);
+}
+
+contactBtn.forEach(item => {
+    item.addEventListener('click', showModal);
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('active');
+
+    document.body.style.overflow = '';
+});
+
+modal.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('active');
+        document.body.style.overflow = '';
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    if(e.code === 'Escape') {
+        modal.classList.remove('active');
+        document.body.style.overflow = '';
+    }
+});
+
+// const modalPopTime = setTimeout(showModal, 3000);
+// function showModalByScroll() {
+//     if ( window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+//         showModal();
+
+//         window.removeEventListener('scroll', showModalByScroll);
+//     }
+// }
+// window.addEventListener('scroll', showModalByScroll);
+
+// const openModalTimer = setTimeout(showModal, 3000);
+
+function showModalBottom() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        showModal();
+        window.removeEventListener('scroll', showModalBottom);
+    }
+}
+window.addEventListener('scroll', showModalBottom);
