7de618710b6be42a09023a484c744ca9ab6cce4d
m.rusakova <m.rusakova@itransition.com>
2021-01-22 22:39:46 +0300

    creat class based cards

--- js/script.js
+++ js/script.js
@@ -95,18 +95,6 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
-// const modalPopTime = setTimeout(showModal, 3000);
-// function showModalByScroll() {
-//     if ( window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-//         showModal();
-
-//         window.removeEventListener('scroll', showModalByScroll);
-//     }
-// }
-// window.addEventListener('scroll', showModalByScroll);
-
-// const openModalTimer = setTimeout(showModal, 3000);
-
 function showModalBottom() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
         showModal();
@@ -114,3 +102,47 @@ function showModalBottom() {
     }
 }
 window.addEventListener('scroll', showModalBottom);
+
+// Nutrition card class
+
+class NutritionCard {
+    constructor(src, alt, title, text, price, parent) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.text = text;
+        this.price = price;
+        this.convert = 2.5;
+        this.convertBYR();
+        this.parent = parent;
+    }
+
+    convertBYR() {
+        this.price = this.price * this.convert;
+    }
+
+    render() {
+        const parentElement = document.querySelector(this.parent),
+            card = document.createElement('div');
+
+        card.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.text}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>
+        `
+
+        parentElement.append(card);
+    };
+}
+
+const premium = new NutritionCard("img/tabs/elite.jpg", 'elite', 'Меню "Премиум"', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 40, '.menu__field .container').render(),
+      fitness = new NutritionCard('img/tabs/vegy.jpg', 'fitness', 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 10, '.menu__field .container').render(),
+      vegy = new NutritionCard('"img/tabs/post.jpg"', 'vegy', 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 20, '.menu__field .container').render();
+
